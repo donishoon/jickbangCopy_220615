1db280ea4de159ccf6aa67cba349600272a114e4
강도훈 <fghfgh3255@gmail.com>
2022-06-15 16:13:38 +0900

    Initial commit

---
+++ app/src/main/java/com/dohn/jickbangcopy_220615/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dohn.jickbangcopy_220615
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
