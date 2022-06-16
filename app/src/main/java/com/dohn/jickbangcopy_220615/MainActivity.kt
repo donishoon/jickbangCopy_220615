3e6d7db4febb303fb3d21e08c3c7872eb28d4d8f
강도훈 <fghfgh3255@gmail.com>
2022-06-16 12:06:56 +0900

    방 데이터 목록 수기로 추가 (임시방편)

--- app/src/main/java/com/dohn/jickbangcopy_220615/MainActivity.kt
+++ app/src/main/java/com/dohn/jickbangcopy_220615/MainActivity.kt
@@ -2,10 +2,27 @@ package com.dohn.jickbangcopy_220615
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dohn.jickbangcopy_220615.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add( RoomData( 8500, "서울시 동대문구", 5, "1번째 방입니다."  ) )
+        mRoomList.add( RoomData( 13400, "서울시 동대문구", 3, "2번째 방입니다."  ) )
+        mRoomList.add( RoomData( 11100, "서울시 서대문구", 0, "3번째 방입니다."  ) )
+        mRoomList.add( RoomData( 444200, "서울시 서대문구", -1, "4번째 방입니다."  ) )
+        mRoomList.add( RoomData( 45000, "서울시 강남구", 10, "5번째 방입니다."  ) )
+        mRoomList.add( RoomData( 25000, "서울시 강남구", 16, "6번째 방입니다."  ) )
+        mRoomList.add( RoomData( 15000, "서울시 강남구", 1, "7번째 방입니다."  ) )
+        mRoomList.add( RoomData( 9000, "서울시 서초구", -3, "8번째 방입니다."  ) )
+        mRoomList.add( RoomData( 1900, "서울시 서초구", 12, "9번째 방입니다."  ) )
+        mRoomList.add( RoomData( 30000, "서울시 서초구", 11, "9번째 방입니다."  ) )
+        mRoomList.add( RoomData( 3000, "서울시 구로구", 7, "10번째 방입니다."  ) )
+
     }
 }
