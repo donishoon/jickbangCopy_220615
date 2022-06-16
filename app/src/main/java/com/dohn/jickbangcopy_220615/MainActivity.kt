7f4641e4be0b5f60bea6137a600cd41f62bd3aed
강도훈 <fghfgh3255@gmail.com>
2022-06-16 15:32:51 +0900

    RoomData 클래스 통째로 첨부 / 활용 - Serializable 활용

--- app/src/main/java/com/dohn/jickbangcopy_220615/MainActivity.kt
+++ app/src/main/java/com/dohn/jickbangcopy_220615/MainActivity.kt
@@ -7,7 +7,11 @@ import com.dohn.jickbangcopy_220615.adapters.RoomAdapter
 import com.dohn.jickbangcopy_220615.datas.RoomData
 import kotlinx.android.synthetic.main.activity_main.*
 
+//  여기는 메인화면
+
 class MainActivity : AppCompatActivity() {
+
+//    방 데이터를 담을 목록 공단
 
     val mRoomList = ArrayList<RoomData>()
 
